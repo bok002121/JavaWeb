be89ba16183d75f2c569fa96ad5461259fed5141
bok002121 <zaq002121@126.com>
2016-08-24 15:56:42 +0800

    42 帅气的 filter

--- priceManger/src/com/cyc/filter/MyFilter2.java
+++ priceManger/src/com/cyc/filter/MyFilter2.java
@@ -8,36 +8,25 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-public class CommentFilter implements Filter
+public class MyFilter2 implements Filter
 {
 
 	public void init(FilterConfig filterConfig) throws ServletException
 	{
 		// TODO Auto-generated method stub
-		String value1 = filterConfig.getInitParameter("hello");
-		System.out.println(value1);
+
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException
 	{
 		// TODO Auto-generated method stub
-		HttpServletRequest req = (HttpServletRequest)request;
-		HttpServletResponse resp = (HttpServletResponse)response;
-		
-		String username = req.getParameter("username");
-		String comment = req.getParameter("comment");
-		
-		username = username.replace("zhang", "li");
-		comment = comment.replace("shengsiyuan", "****");
-		
-		req.setAttribute("username", username);
-		req.setAttribute("comment",comment);
+        System.out.println("myFilter2 before chain.doFilter");
 		
 		chain.doFilter(request, response);
+		
+		System.out.println("myFilter2 after chain.doFilter");
 	}
 
 	public void destroy()
